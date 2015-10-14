a269139501c9866fdc0c07dee94a325fe6cc3a46
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-14 12:42:36 +0100

    Add design to Main activity

---
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/IntervalSettingsFragment.java
@@ -0,0 +1,9 @@
+package checkpoint.andela.com.productivitytracker;
+
+import android.app.Fragment;
+
+/**
+ * Created by andela-cj on 14/10/2015.
+ */
+public class IntervalSettingsFragment extends Fragment {
+}
--- app/src/main/java/checkpoint/andela/com/productivitytracker/StartFragment.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/StartFragment.java
@@ -1,8 +1,11 @@
 package checkpoint.andela.com.productivitytracker;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,10 +22,20 @@ public class StartFragment  extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.start_fragment, container);
         startButton = (ImageButton)v.findViewById(R.id.playButton);
+        startButton.setOnClickListener(onClickListener);
         return v;
     }
 
     public ImageButton getStartButton() {
         return startButton;
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FragmentManager fm = getActivity().getFragmentManager();
+            IntervalSettingsFragment intervalSettingsFragment = new IntervalSettingsFragment();
+            fm.beginTransaction().add(R.id.mainActivity, intervalSettingsFragment).commit();
+        }
+    };
 }
