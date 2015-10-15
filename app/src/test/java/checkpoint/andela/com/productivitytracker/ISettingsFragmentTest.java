ccfa5c87293c593c8b84527c037a3b0f5fbd0769
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-15 10:14:51 +0100

    Add test for settingFragment

--- app/src/test/java/checkpoint/andela/com/productivitytracker/ISettingsFragmentTest.java
+++ app/src/test/java/checkpoint/andela/com/productivitytracker/ISettingsFragmentTest.java
@@ -1,7 +1,56 @@
 package checkpoint.andela.com.productivitytracker;
 
+import android.app.FragmentTransaction;
+import android.widget.RelativeLayout;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.FragmentTestUtil;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Created by andela-cj on 14/10/2015.
  */
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class ISettingsFragmentTest {
+    private MainActivity mActivity;
+    private SeekArc arc;
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = Robolectric.buildActivity(MainActivity.class).create().start().get();
+        FragmentTransaction ft = mActivity.getFragmentManager().beginTransaction();
+        IntervalSettingsFragment iSF= new IntervalSettingsFragment();
+        ft.add(R.id.mainActivity, iSF).commit();
+    }
+
+    @Test
+    public void testFragmentTransition() throws Exception {
+        assertThat(mActivity.findViewById(R.id.intervalLayout)).isNotNull();
+    }
+
+    @Test
+    public void testSeekBarIsNotNull() {
+        assertThat(mActivity.findViewById(R.id.intervalLayout)).isNotNull();
+        arc = (SeekArc) mActivity.findViewById(R.id.seekArc);
+        assertThat(arc).isNotNull();
+    }
+
+    @Test
+    public void testSeekArcAttributes() {
+        arc = (SeekArc) mActivity.findViewById(R.id.seekArc);
+        assertThat(arc).isNotNull();
+        assertThat(arc.getStartAngle()).isEqualTo(0);
+        assertThat(arc.getArcRotation()).isEqualTo(30);
+        assertThat(arc.getSweepAngle()).isEqualTo(330);
+        assertThat(arc.getArcWidth()).isEqualTo(2);
+        assertThat(arc.getProgressWidth()).isEqualTo(4);
+    }
 }
