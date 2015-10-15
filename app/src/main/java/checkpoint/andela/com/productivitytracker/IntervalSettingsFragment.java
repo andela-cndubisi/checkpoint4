729846d9a7d8ef649d0b79be7f522f91023e59de
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-15 10:12:48 +0100

    Create Settings Fragment

--- app/src/main/java/checkpoint/andela/com/productivitytracker/IntervalSettingsFragment.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/IntervalSettingsFragment.java
@@ -1,9 +1,28 @@
 package checkpoint.andela.com.productivitytracker;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by andela-cj on 14/10/2015.
  */
 public class IntervalSettingsFragment extends Fragment {
+
+    SeekArc seekBar;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.interval_setting_fragment, null);
+        seekBar = (SeekArc) v.findViewById(R.id.seekArc);
+        return v;
+    }
+
+    public SeekArc getSeekBar(){
+        return seekBar;
+    }
 }
