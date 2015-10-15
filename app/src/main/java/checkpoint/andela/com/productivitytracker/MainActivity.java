de4b3ffd33b9fac542350a35c602cdea90b58fec
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-15 10:17:23 +0100

    Add fragment programatically

--- app/src/main/java/checkpoint/andela/com/productivitytracker/MainActivity.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/MainActivity.java
@@ -1,13 +1,23 @@
 package checkpoint.andela.com.productivitytracker;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
+    private StartFragment startFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (startFragment == null){
+            startFragment = new StartFragment();
+            getFragmentManager().beginTransaction().add(R.id.container, startFragment).commit();
+        }
+    }
+
+    public StartFragment getStartFragment() {
+        return startFragment;
     }
 }
