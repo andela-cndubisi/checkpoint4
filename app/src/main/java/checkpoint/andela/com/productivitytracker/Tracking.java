a648d382411d6b856be99cba2ea6c5e7c45d885c
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-15 14:41:45 +0100

    Remove boiler plate cold

---
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/Tracking.java
@@ -0,0 +1,27 @@
+package checkpoint.andela.com.productivitytracker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+
+public class Tracking extends Fragment {
+    private TextView durationSpent;
+    private ImageButton pause;
+    private TextView numberofLocations;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tracking_fragment, container, false);
+        durationSpent = (TextView)v.findViewById(R.id.duration);
+        pause = (ImageButton) v.findViewById(R.id.pause);
+        numberofLocations = (TextView) v.findViewById(R.id.number_of_locations);
+        return v;
+    }
+}
