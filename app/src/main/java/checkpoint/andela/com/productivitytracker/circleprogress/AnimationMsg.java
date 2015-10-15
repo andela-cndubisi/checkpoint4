fd9c828cb04fa8e0a987666e6cc6bf920e38e6f2
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-15 14:41:09 +0100

    Add cirle progess bar library

---
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/circleprogress/AnimationMsg.java
@@ -0,0 +1,11 @@
+package checkpoint.andela.com.productivitytracker.circleprogress;
+
+enum AnimationMsg {
+
+   START_SPINNING,
+   STOP_SPINNING,
+   SET_VALUE,
+   SET_VALUE_ANIMATED,
+   TICK
+
+}
