9e07d1e6c745d1e37b305e47026fbb99fd647b7c
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-19 18:01:45 +0100

    add new activity history

--- app/src/main/java/checkpoint/andela/com/productivitytracker/activities/HistoryActivity.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/activities/HistoryActivity.java
@@ -2,14 +2,54 @@ package checkpoint.andela.com.productivitytracker.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import checkpoint.andela.com.productivitytracker.R;
+import checkpoint.andela.com.productivitytracker.RVAdapter;
+
 
 public class HistoryActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    RecyclerView rv ;
+    private List<History> persons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        rv =  (RecyclerView)findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        initializeData();
+        RVAdapter adapter = new RVAdapter(persons);
+        rv.setAdapter(adapter);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.left);
+        toolbar.setTitle("History");
+
+        setSupportActionBar(toolbar);
     }
+
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new History("Emma Wilson", "23 years old"));
+        persons.add(new History("Lavery Maiss", "25 years old"));
+        persons.add(new History("Lillie Watts", "35 years old"));
+    }
+
+    public class History {
+        public String date;
+        public String position;
+
+        History(String date, String numberOfPosition) {
+            this.date = date;
+            this.position = numberOfPosition;
+        }
+    }
+
 }
