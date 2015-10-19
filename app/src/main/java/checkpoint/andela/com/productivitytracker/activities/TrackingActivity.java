22ef46f8d7cc497edb200b7596cd038922a9393a
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-19 09:15:38 +0100

    Add type face and timer

--- app/src/main/java/checkpoint/andela/com/productivitytracker/activities/TrackingActivity.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/activities/TrackingActivity.java
@@ -1,7 +1,11 @@
 package checkpoint.andela.com.productivitytracker.activities;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -21,17 +25,31 @@ public class TrackingActivity extends Activity{
     private Button playButton;
     private CircleProgressView progressView;
     private GoogleLocationManager locationManager;
+    long starttime = 0L;
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedtime = 0L;
+    int t = 1;
+    int hr = 0;
+    int secs = 0;
+    int mins = 0;
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tracking_fragment);
+        Typeface faceLight= Typeface.createFromAsset(getAssets(), "fonts/Gotham-Light.ttf");
+        Typeface faceMedium= Typeface.createFromAsset(getAssets(), "fonts/Gotham-Medium.ttf");
         locationManager = new GoogleLocationManager(this);
         durationSpent = (TextView)findViewById(R.id.duration);
         pause = (ImageButton) findViewById(R.id.pause_button);
         stopButton = (Button)findViewById(R.id.stop_button);
         playButton = (Button)findViewById(R.id.play_button);
         numberofLocations = (TextView) findViewById(R.id.number_of_locations);
+        ((TextView) (findViewById(R.id.location_count_label))).setTypeface(faceMedium);
+        durationSpent.setTypeface(faceLight);
+        numberofLocations.setTypeface(faceLight);
         progressView = (CircleProgressView) findViewById(R.id.circleView);
         setImageButtonOnClickListener();
     }
@@ -46,25 +64,31 @@ public class TrackingActivity extends Activity{
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            int mode = getResources().getConfiguration().orientation;
 
-                findViewById(R.id.progress_circle).setVisibility(View.GONE);
-                findViewById(R.id.action).setVisibility(View.VISIBLE);
-            if (mode != 2) {
-                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) playButton.getLayoutParams();
-                params.addRule(11, -1);
-                playButton.setLayoutParams(params);
+       if (v.getId() == R.id.pause_button){
+           int mode = getResources().getConfiguration().orientation;
 
-                RelativeLayout.LayoutParams params2 = (RelativeLayout.LayoutParams) stopButton.getLayoutParams();
-                params2.addRule(9, -1);
-                stopButton.setLayoutParams(params2);
+           findViewById(R.id.progress_circle).setVisibility(View.GONE);
+           findViewById(R.id.action).setVisibility(View.VISIBLE);
+           if (mode != 2) {
+               RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) playButton.getLayoutParams();
+               params.addRule(11, -1);
+               playButton.setLayoutParams(params);
 
-            }
-                if (v.getId() == R.id.stop_button) {
+               RelativeLayout.LayoutParams params2 = (RelativeLayout.LayoutParams) stopButton.getLayoutParams();
+               params2.addRule(9, -1);
+               stopButton.setLayoutParams(params2);
+           }
+           timeSwapBuff += timeInMilliseconds;
+           handler.removeCallbacks(updateTimer);
+           t = 1;
+       }
 
-                } else if (v.getId() == R.id.play_button) {
+        if (v.getId() == R.id.stop_button) {
 
-                }
+        } else if (v.getId() == R.id.play_button) {
+
+        }
         }
     };
 
@@ -86,6 +110,27 @@ public class TrackingActivity extends Activity{
     @Override
     public void onStart() {
         super.onStart();
+        starttime = SystemClock.uptimeMillis();
+        handler.postDelayed(updateTimer, 0);
+        t = 0;
         locationManager.connect();
     }
+
+
+    public Runnable updateTimer = new Runnable() {
+
+        public void run() {
+
+            timeInMilliseconds = SystemClock.uptimeMillis() - starttime;
+            updatedtime = timeSwapBuff + timeInMilliseconds;
+            secs = (int) (updatedtime / 1000);
+            hr = mins / 60;
+            mins = secs / 60;
+            secs = secs % 60;
+            durationSpent.setText(String.format("%02d:%02d:%02d",0,mins, secs));
+            handler.postDelayed(this, 0);
+        }
+
+    };
+
 }
