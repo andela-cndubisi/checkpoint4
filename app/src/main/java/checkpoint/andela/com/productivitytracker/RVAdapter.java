c35ab27fb80aaf9d5bf1757cd5d9a223836fccd4
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-19 18:02:10 +0100

    Add card view to history

---
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/RVAdapter.java
@@ -0,0 +1,55 @@
+package checkpoint.andela.com.productivitytracker;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import checkpoint.andela.com.productivitytracker.activities.HistoryActivity;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.HistoryViewHolder>{
+
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public HistoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false);
+        HistoryViewHolder vh = new HistoryViewHolder(v);
+        return vh;    }
+
+    public void onBindViewHolder(HistoryViewHolder holder, int position) {
+        holder.date.setText(item.get(position).date);
+        holder.positions.setText(item.get(position).position);
+
+    }
+    public int getItemCount() {
+        return item.size();
+    }
+
+    public static class HistoryViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView date;
+        TextView positions;
+
+        HistoryViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv);
+            date = (TextView)itemView.findViewById(R.id.date_holder);
+            positions = (TextView)itemView.findViewById(R.id.no_of_position);
+        }
+
+    }
+
+    List<HistoryActivity.History> item;
+
+    public RVAdapter(List<HistoryActivity.History> item){
+        this.item = item;
+    }
+}
