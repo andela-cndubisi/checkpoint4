f0faf4d8bdf550952e2a755dc37cb0844ae68846
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-19 13:27:15 +0100

    Add Layouts for history activity

---
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/activities/HistoryActivity.java
@@ -0,0 +1,15 @@
+package checkpoint.andela.com.productivitytracker.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import checkpoint.andela.com.productivitytracker.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
