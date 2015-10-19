1630bafdca631fba61497c3043bd45af722daf9e
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-19 09:17:01 +0100

    Add shared preferences

--- app/src/main/java/checkpoint/andela/com/productivitytracker/fragments/StartFragment.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/fragments/StartFragment.java
@@ -2,32 +2,42 @@ package checkpoint.andela.com.productivitytracker.fragments;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import checkpoint.andela.com.productivitytracker.R;
 import checkpoint.andela.com.productivitytracker.activities.TrackingActivity;
+import static checkpoint.andela.com.productivitytracker.fragments.SettingsFragment.INTERVAL;
 
 /**
  * Created by andela-cj on 14/10/2015.
  */
-public class StartFragment  extends Fragment{
+public class StartFragment  extends Fragment implements SettingsFragment.iSettings {
     private ImageButton startButton;
     private Button interval;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.start_fragment, container, false);
+        Typeface face= Typeface.createFromAsset(getActivity().getAssets(), "fonts/Gotham-Light.ttf");
+
         startButton = (ImageButton)v.findViewById(R.id.start_button);
         interval = (Button)v.findViewById(R.id.interval);
-
+        ((TextView)v.findViewById(R.id.description)).setTypeface(face);
+        interval.setTypeface(face);
         startButton.setOnClickListener(onClickListener);
         interval.setOnClickListener(onClickListener);
+
         return v;
     }
 
@@ -47,12 +57,30 @@ public class StartFragment  extends Fragment{
 
     };
 
-    private void setInterval(){
-        IntervalSettingsFragment settings = new IntervalSettingsFragment();
-        settings.show(getFragmentManager(), "My Settings Dialog");
+    @Override
+    public void onStart() {
+        SharedPreferences sharedpreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        int currentInterval = sharedpreferences.getInt(INTERVAL, 5);
+        updateSettings(String.format("%d",currentInterval));
+
+        super.onStart();
     }
+
+    private void setInterval(){
+        SettingsFragment settings = new SettingsFragment();
+        settings.setIntervalDelegate(this);
+        if (!settings.isVisible() )
+            settings.show(getFragmentManager(), "My Settings Dialog");
+
+    }
+
     private void start() {
         Intent a = new Intent(getActivity(), TrackingActivity.class);
         startActivity(a);
+    }
+
+
+    public void updateSettings(String progress) {
+        interval.setText((progress + " minutes"));
     }
 }
