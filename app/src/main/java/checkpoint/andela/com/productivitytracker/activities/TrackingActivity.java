139a4360221a47d6c48013ded78257d5b4c393fc
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-20 11:38:11 +0100

    Add Connection

--- app/src/main/java/checkpoint/andela/com/productivitytracker/activities/TrackingActivity.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/activities/TrackingActivity.java
@@ -2,13 +2,15 @@ package checkpoint.andela.com.productivitytracker.activities;
 
 import android.app.Activity;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.SystemClock;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -25,15 +27,13 @@ public class TrackingActivity extends Activity{
     private TextView durationSpent;
     private ImageButton pause;
     private TextView numberofLocations;
-    private Button stopButton;
-    private Button playButton;
+    private ImageButton stopButton;
+    private ImageButton playButton;
     private CircleProgressView progressView;
     private GoogleLocationManager locationManager;
     int interval = 0;
-    long starttime = 0L;
-    long timeInMilliseconds = 0L;
-    private long oldSystemTime = 0L;
-    long presentTime = 0L;
+    private Intent trackerIntent;
+    TimerService trackerService;
 
 
     @Override
@@ -46,8 +46,8 @@ public class TrackingActivity extends Activity{
         locationManager = new GoogleLocationManager(this);
         durationSpent = (TextView)findViewById(R.id.duration);
         pause = (ImageButton) findViewById(R.id.pause_button);
-        stopButton = (Button)findViewById(R.id.stop_button);
-        playButton = (Button)findViewById(R.id.play_button);
+        stopButton = (ImageButton)findViewById(R.id.stop_button);
+        playButton = (ImageButton)findViewById(R.id.play_button);
         numberofLocations = (TextView) findViewById(R.id.number_of_locations);
         ((TextView) (findViewById(R.id.location_count_label))).setTypeface(faceMedium);
         durationSpent.setTypeface(faceLight);
@@ -55,38 +55,56 @@ public class TrackingActivity extends Activity{
         progressView = (CircleProgressView) findViewById(R.id.circleView);
         progressView.setSeekModeEnabled(false);
         progressView.setValue(0);
-
-        registerReceiver(mBroadCastReceiver, new IntentFilter("com.andela.checkpoint"));
-
-        if(savedInstanceState !=null){
-            starttime = savedInstanceState.getLong("Time");
+        if (savedInstanceState !=null){
+            trackerIntent = savedInstanceState.getParcelable("intent");
         }
         setImageButtonOnClickListener();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        outState.putLong("Time",starttime);
+        outState.putParcelable("intent",trackerIntent);
         super.onSaveInstanceState(outState);
-
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        if (savedInstanceState !=null)
-            starttime = savedInstanceState.getLong("Time");
+        trackerIntent = savedInstanceState.getParcelable("intent");
         super.onRestoreInstanceState(savedInstanceState);
-
     }
 
     @Override
     protected void onResume() {
+        bindService(trackerIntent, mConnection, Context.BIND_AUTO_CREATE);
+        registerReceiver(mBroadCastReceiver, new IntentFilter("com.andela.checkpoint"));
         super.onResume();
     }
 
     @Override
+    public void onStop() {
+        locationManager.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
     protected void onPause() {
+        unregisterReceiver(mBroadCastReceiver);
+        unbindService(mConnection);
         super.onPause();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        locationManager.connect();
+        if (trackerIntent == null){
+            trackerIntent = new Intent(this, TimerService.class);
+            bindService(trackerIntent, mConnection, Context.BIND_AUTO_CREATE);
+            trackerIntent.putExtra("Interval", interval);
+            startService(trackerIntent);
+        }
     }
 
     private void setImageButtonOnClickListener(){
@@ -103,31 +121,30 @@ public class TrackingActivity extends Activity{
             RelativeLayout.LayoutParams params2 = (RelativeLayout.LayoutParams) stopButton.getLayoutParams();
 
             if (v.getId() == R.id.pause_button){
-           int mode = getResources().getConfiguration().orientation;
+               int mode = getResources().getConfiguration().orientation;
 
-           findViewById(R.id.progress_circle).setVisibility(View.GONE);
-           findViewById(R.id.action).setVisibility(View.VISIBLE);
-           if (mode != 2) {
-               params.addRule(11, -1);
-               playButton.setLayoutParams(params);
+               findViewById(R.id.progress_circle).setVisibility(View.GONE);
+               findViewById(R.id.action).setVisibility(View.VISIBLE);
+               if (mode != 2) {
+                   params.addRule(11, -1);
+                   playButton.setLayoutParams(params);
 
-               params2.addRule(9, -1);
-               stopButton.setLayoutParams(params2);
-           }
-           oldSystemTime = SystemClock.uptimeMillis();
-       }
+                   params2.addRule(9, -1);
+                   stopButton.setLayoutParams(params2);
+               }
+                trackerService.pauseTimer();
+            }
 
-        if (v.getId() == R.id.stop_button) {
-            startHistory();
-        } else if (v.getId() == R.id.play_button) {
-            params.addRule(11, 0);
-            params2.addRule(9, 0);
+            if (v.getId() == R.id.stop_button) {
+                startHistory();
+            } else if (v.getId() == R.id.play_button) {
+                params.addRule(11, 0);
+                params2.addRule(9, 0);
 
-            findViewById(R.id.progress_circle).setVisibility(View.VISIBLE);
-            findViewById(R.id.action).setVisibility(View.GONE);
-            presentTime += SystemClock.uptimeMillis() - oldSystemTime;
-
-        }
+                findViewById(R.id.progress_circle).setVisibility(View.VISIBLE);
+                findViewById(R.id.action).setVisibility(View.GONE);
+                trackerService.resumeTimer();
+            }
         }
     };
 
@@ -139,24 +156,11 @@ public class TrackingActivity extends Activity{
         }
     };
 
-    @Override
-    public void onStop() {
-        locationManager.disconnect();
-        super.onStop();
-    }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        locationManager.connect();
-
-        Intent intent = new Intent(this, TimerService.class);
-        intent.putExtra("Interval",interval);
-        startService(intent);
-    }
 
     public void startHistory(){
         Intent a = new Intent(this, HistoryActivity.class);
+
         startActivity(a);
         this.finish();
     }
@@ -174,4 +178,18 @@ public class TrackingActivity extends Activity{
         durationSpent.setText(time);
         progressView.setValue(percent);
     }
+
+
+    private ServiceConnection mConnection = new ServiceConnection(){
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            TimerService.TimerBinder binder = (TimerService.TimerBinder) service;
+            trackerService = binder.getService();
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            //
+        }
+    };
 }
