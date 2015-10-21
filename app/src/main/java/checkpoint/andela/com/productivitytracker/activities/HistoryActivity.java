f60cb92bf513efdaf15e1d4d066dbb2bb67e08af
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-21 17:28:10 +0100

    Allow dummy data insertion

--- app/src/main/java/checkpoint/andela/com/productivitytracker/activities/HistoryActivity.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/activities/HistoryActivity.java
@@ -1,24 +1,26 @@
 package checkpoint.andela.com.productivitytracker.activities;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import checkpoint.andela.com.productivitytracker.DateCount;
 import checkpoint.andela.com.productivitytracker.R;
 import checkpoint.andela.com.productivitytracker.RVAdapter;
+import checkpoint.andela.com.productivitytracker.data.ProductivityContract;
+import checkpoint.andela.com.productivitytracker.data.ProductivityDBHelper;
 
 
 public class HistoryActivity extends AppCompatActivity {
     RecyclerView rv ;
-    private List<History> persons;
+    private List<DateCount> persons = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,26 +29,39 @@ public class HistoryActivity extends AppCompatActivity {
         rv =  (RecyclerView)findViewById(R.id.rv);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rv.setLayoutManager(llm);
-        initializeData();
+        insertDummyData();
+        populateData();
         RVAdapter adapter = new RVAdapter(persons);
         rv.setAdapter(adapter);
-    }
-    private void initializeData(){
-        persons = new ArrayList<>();
-        persons.add(new History("Emma Wilson", "23 years old"));
-        persons.add(new History("Lavery Maiss", "25 years old"));
-        persons.add(new History("Lillie Watts", "35 years old"));
+        rv.setOnClickListener(onClickListener);
     }
 
 
-    public class History {
-        public String date;
-        public String position;
+    RecyclerView.OnClickListener onClickListener = new RecyclerView.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-        History(String date, String numberOfPosition) {
-            this.date = date;
-            this.position = numberOfPosition;
         }
+    };
+
+    private void populateData() {
+        ProductivityDBHelper dbHelper = new ProductivityDBHelper(getApplicationContext());
+        persons = dbHelper.getDateWithCount();
     }
+
+    public void insertDummyData(){
+        ProductivityDBHelper dbHelper =  new ProductivityDBHelper(getApplicationContext());
+        SQLiteDatabase db1 = dbHelper.getWritableDatabase();
+        ContentValues vl = new ContentValues();
+        vl.put(ProductivityContract.LocationEntry.COLUMN_DATE_TEXT,"2015-08-09");
+        vl.put(ProductivityContract.LocationEntry.COLUMN_CITY_NAME,"Yaba, Lagos");
+        vl.put(ProductivityContract.LocationEntry.COLUMN_LONGITUDE,3.380699);
+        vl.put(ProductivityContract.LocationEntry.COLUMN_LATITUDE, 6.518296);
+        long count = db1.insert(ProductivityContract.LocationEntry.TABLE_NAME, null, vl);
+        db1.close();
+    }
+
+
+
 
 }
