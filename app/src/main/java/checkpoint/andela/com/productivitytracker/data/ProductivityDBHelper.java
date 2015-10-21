4e677c24d0053347dadb970d39259dec96376c1d
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-21 20:54:12 +0100

    Add clean up query

--- app/src/main/java/checkpoint/andela/com/productivitytracker/data/ProductivityDBHelper.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/data/ProductivityDBHelper.java
@@ -40,67 +40,31 @@ public class ProductivityDBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-    public ArrayList<String> getUniqueLocationDateQueryArg() {
-        ArrayList<String> dates = new ArrayList<>();
-        try {
-            SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
-            Cursor cursor = sqLiteDatabase.query(true,
-                    LocationEntry.TABLE_NAME
-                    , new String[]{LocationEntry.COLUMN_DATE_TEXT}
+
+    public ArrayList<DateCount> getDateWithCount(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ArrayList<DateCount> list = new ArrayList<>();
+        Cursor cursor = db.query(true
+                    , LocationEntry.TABLE_NAME
+                    , new String[]{"Count("+LocationEntry.COLUMN_DATE_TEXT+")", LocationEntry.COLUMN_DATE_TEXT}
                     , null
                     , null
                     , LocationEntry.COLUMN_DATE_TEXT
                     , null
                     , null
-                    , null );
-            if (cursor.moveToFirst()) {
-                while (!cursor.isAfterLast()) {
-                    String date = cursor.getString(cursor.getColumnIndex(LocationEntry.COLUMN_DATE_TEXT));
-                    dates.add(date);
-                    cursor.moveToNext();
-                }
-                cursor.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return dates;
-    }
-
-
-
-    public ArrayList<DateCount> getDateWithCount(){
-        SQLiteDatabase db = this.getReadableDatabase();
-        ArrayList<String> a = getUniqueLocationDateQueryArg();
-        ArrayList<DateCount> list = new ArrayList<>();
-        Cursor cursor =null;
-        for (int i =0; i< a.size(); i++){
-             cursor = db.query(false
-                    , LocationEntry.TABLE_NAME
-                    , null
-                    , LocationEntry.COLUMN_DATE_TEXT + " =?"
-                    , new String[]{a.get(i)}
-                    , null
-                    , null
-                    , null
                     , null /* limit */);
 
-                DateCount history = null;
-                if (cursor.moveToNext()) {
-                    history = getHistoryFromCursor(cursor);
-                    list.add(history);
-                }
+
+        for (int i =0; i< cursor.getCount(); i++){
+             if (cursor.moveToNext()) {
+                 int countIdx = cursor.getColumnIndex("Count("+LocationEntry.COLUMN_DATE_TEXT+")");
+                 int dateIdx = cursor.getColumnIndex(LocationEntry.COLUMN_DATE_TEXT);
+                 DateCount history = new DateCount(cursor.getString(dateIdx),cursor.getInt(countIdx),0);
+                list.add(history);
+            }
         }
-        if (cursor!=null) cursor.close();
+        cursor.close();
         db.close();
         return list;
     }
-
-    private DateCount getHistoryFromCursor(Cursor cursor) {
-        int dateIdx = cursor.getColumnIndex(LocationEntry.COLUMN_DATE_TEXT);
-        DateCount history = new DateCount(
-                cursor.getString(dateIdx),cursor.getCount());
-        return history;
-    }
-
 }
