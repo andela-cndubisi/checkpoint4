2f267caeb6c55472df0e3ec89a58eea8cbbcb9e7
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-21 17:31:32 +0100

    Implement helper methods

--- app/src/main/java/checkpoint/andela/com/productivitytracker/data/ProductivityDBHelper.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/data/ProductivityDBHelper.java
@@ -1,11 +1,15 @@
 package checkpoint.andela.com.productivitytracker.data;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
-import static checkpoint.andela.com.productivitytracker.data.ProductivityContract.*;
+import java.util.ArrayList;
+import java.util.Map;
+
+import checkpoint.andela.com.productivitytracker.DateCount;
+
 import static checkpoint.andela.com.productivitytracker.data.ProductivityContract.LocationEntry;
 
 /**
@@ -36,4 +40,67 @@ public class ProductivityDBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+    public ArrayList<String> getUniqueLocationDateQueryArg() {
+        ArrayList<String> dates = new ArrayList<>();
+        try {
+            SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+            Cursor cursor = sqLiteDatabase.query(true,
+                    LocationEntry.TABLE_NAME
+                    , new String[]{LocationEntry.COLUMN_DATE_TEXT}
+                    , null
+                    , null
+                    , LocationEntry.COLUMN_DATE_TEXT
+                    , null
+                    , null
+                    , null );
+            if (cursor.moveToFirst()) {
+                while (!cursor.isAfterLast()) {
+                    String date = cursor.getString(cursor.getColumnIndex(LocationEntry.COLUMN_DATE_TEXT));
+                    dates.add(date);
+                    cursor.moveToNext();
+                }
+                cursor.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dates;
+    }
+
+
+
+    public ArrayList<DateCount> getDateWithCount(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ArrayList<String> a = getUniqueLocationDateQueryArg();
+        ArrayList<DateCount> list = new ArrayList<>();
+        Cursor cursor =null;
+        for (int i =0; i< a.size(); i++){
+             cursor = db.query(false
+                    , LocationEntry.TABLE_NAME
+                    , null
+                    , LocationEntry.COLUMN_DATE_TEXT + " =?"
+                    , new String[]{a.get(i)}
+                    , null
+                    , null
+                    , null
+                    , null /* limit */);
+
+                DateCount history = null;
+                if (cursor.moveToNext()) {
+                    history = getHistoryFromCursor(cursor);
+                    list.add(history);
+                }
+        }
+        if (cursor!=null) cursor.close();
+        db.close();
+        return list;
+    }
+
+    private DateCount getHistoryFromCursor(Cursor cursor) {
+        int dateIdx = cursor.getColumnIndex(LocationEntry.COLUMN_DATE_TEXT);
+        DateCount history = new DateCount(
+                cursor.getString(dateIdx),cursor.getCount());
+        return history;
+    }
+
 }
