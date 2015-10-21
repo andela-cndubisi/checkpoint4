0bf650b7da3ecbf2a58b1e641a21870ba472612b
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-21 23:26:42 +0100

    Add interval

--- app/src/main/java/checkpoint/andela/com/productivitytracker/DateCount.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/DateCount.java
@@ -1,19 +1,20 @@
 package checkpoint.andela.com.productivitytracker;
 
 import android.graphics.PointF;
+import android.support.annotation.Nullable;
 
 /**
  * Created by andela-cj on 21/10/2015.
  */
 public  class DateCount {
+    public long timespent;
     public String date;
     public String position;
 
-    public DateCount(String date, int numberOfPosition) {
+    public DateCount(String date, int numberOfPosition, long timespent) {
         this.date = date;
         this.position = formatPosition(numberOfPosition);
-
-
+        this.timespent = timespent;
     }
     public String formatPosition(int p){
         return String.format("%d records",p);
