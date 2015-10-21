1181660f5e2f721eebe69f6241ac4e694dd6346c
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-21 23:31:33 +0100

    Create MapActivity

---
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/activities/MapActivity.java
@@ -0,0 +1,16 @@
+
+package checkpoint.andela.com.productivitytracker.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import checkpoint.andela.com.productivitytracker.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
