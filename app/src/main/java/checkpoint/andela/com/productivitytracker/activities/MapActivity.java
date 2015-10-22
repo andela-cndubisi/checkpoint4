1050b95e214a5b88f56bc03cf6521f1f34a236a1
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-22 16:31:19 +0100

    Add map Activity

--- app/src/main/java/checkpoint/andela/com/productivitytracker/activities/MapActivity.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/activities/MapActivity.java
@@ -1,16 +1,54 @@
 
 package checkpoint.andela.com.productivitytracker.activities;
 
+import android.graphics.PointF;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
+
 import checkpoint.andela.com.productivitytracker.R;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private ArrayList<PointF> locations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        try {
+            locations = (ArrayList<PointF>) getIntent().getSerializableExtra("list");
+            setTitle(getIntent().getStringExtra("title"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng latLng = null;
+        for (PointF location : locations) {
+            latLng = new LatLng((double)location.x, (double)location.y);
+            googleMap.addMarker(new MarkerOptions().position(latLng));
+        }
+        CameraUpdate updateFactory = CameraUpdateFactory.newLatLngZoom(latLng, 16);
+        googleMap.moveCamera(updateFactory);
+        googleMap.animateCamera(updateFactory);
     }
 }
