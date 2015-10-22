7cb282d0a3992c337ae33e9a701ccfc246099007
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-22 12:19:03 +0100

    Package Restructure

---
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/lib/circleprogress/TextMode.java
@@ -0,0 +1,16 @@
+package checkpoint.andela.com.productivitytracker.lib.circleprogress;
+
+public enum TextMode {
+   /**
+    * Show specified text
+    */
+   TEXT,
+   /**
+    * Show percent of current value from max value
+    */
+   PERCENT,
+   /**
+    * Show current value
+    */
+   VALUE
+}
