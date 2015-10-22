88c6d7e0afe863c1a55c566a68e5b4e98b3a4b84
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-22 12:19:52 +0100

    Restructure

---
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/lib/circleprogress/AnimationMsg.java
@@ -0,0 +1,11 @@
+package checkpoint.andela.com.productivitytracker.lib.circleprogress;
+
+enum AnimationMsg {
+
+   START_SPINNING,
+   STOP_SPINNING,
+   SET_VALUE,
+   SET_VALUE_ANIMATED,
+   TICK
+
+}
