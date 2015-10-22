718fb8abcf06dab0e2fc5d7715305affad2fe1c6
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-22 16:27:06 +0100

    Set click listeners for items

--- app/src/main/java/checkpoint/andela/com/productivitytracker/RVAdapter.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/RVAdapter.java
@@ -1,5 +1,6 @@
 package checkpoint.andela.com.productivitytracker;
 
+import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +9,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.util.List;
+
+import checkpoint.andela.com.productivitytracker.activities.MapActivity;
+import checkpoint.andela.com.productivitytracker.data.ProductivityDBHelper;
 
 public class RVAdapter extends RecyclerView.Adapter<RVAdapter.HistoryViewHolder>{
 
@@ -19,8 +23,25 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.HistoryViewHolder>
 
     public HistoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false);
+        v.setOnClickListener(onClickListener);
         HistoryViewHolder vh = new HistoryViewHolder(v);
         return vh;    }
+
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String time = ((TextView) v.findViewById(R.id.time_spent)).getText().toString();
+            if (time.isEmpty()){
+                TextView date = (TextView)v.findViewById(R.id.date_holder);
+                String name = date.getText().toString();
+                Intent mappedHistoryIntent = new Intent(v.getContext().getApplicationContext(), MapActivity.class);
+                mappedHistoryIntent.putExtra("list", new ProductivityDBHelper(v.getContext().getApplicationContext()).getDateLocations(name));
+                mappedHistoryIntent.putExtra("title", name);
+                v.getContext().startActivity(mappedHistoryIntent);
+            }
+        }
+    };
 
     public void onBindViewHolder(HistoryViewHolder holder, int position) {
         holder.date.setText(item.get(position).date);
@@ -34,7 +55,9 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.HistoryViewHolder>
         return item.size();
     }
 
-    public static class HistoryViewHolder extends RecyclerView.ViewHolder {
+    public static class HistoryViewHolder extends RecyclerView.ViewHolder   {
+
+
         CardView cv;
         TextView date;
         TextView positions;
@@ -47,7 +70,6 @@ public class RVAdapter extends RecyclerView.Adapter<RVAdapter.HistoryViewHolder>
             positions = (TextView)itemView.findViewById(R.id.no_of_position);
             timespent = (TextView)itemView.findViewById(R.id.time_spent);
         }
-
     }
 
     List<DateCount> item;
