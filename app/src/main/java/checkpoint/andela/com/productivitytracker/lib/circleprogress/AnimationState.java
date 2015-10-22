0a5c03ab34a3eba803b53e422935d79de26f471e
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-22 12:19:31 +0100

    Package restructure

---
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/lib/circleprogress/AnimationState.java
@@ -0,0 +1,11 @@
+package checkpoint.andela.com.productivitytracker.lib.circleprogress;
+
+public enum AnimationState {
+
+    IDLE,
+    SPINNING,
+    END_SPINNING,
+    END_SPINNING_START_ANIMATING,
+    START_ANIMATING_AFTER_SPINNING, ANIMATING
+
+}
