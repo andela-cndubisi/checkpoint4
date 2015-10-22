3f82b972522ef9f659b926b8907f563562e31712
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-22 16:33:08 +0100

    Add additional helper queries

--- app/src/main/java/checkpoint/andela/com/productivitytracker/data/ProductivityDBHelper.java
+++ app/src/main/java/checkpoint/andela/com/productivitytracker/data/ProductivityDBHelper.java
@@ -1,11 +1,18 @@
 package checkpoint.andela.com.productivitytracker.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.PointF;
+import android.location.Location;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 
 import checkpoint.andela.com.productivitytracker.DateCount;
@@ -47,14 +54,14 @@ public class ProductivityDBHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         ArrayList<DateCount> list = new ArrayList<>();
         Cursor cursor = db.query(true
-                    , LocationEntry.TABLE_NAME
-                    , new String[]{COLUMN_SELECTION, LocationEntry.COLUMN_DATE_TEXT}
-                    , null
-                    , null
-                    , LocationEntry.COLUMN_DATE_TEXT
-                    , null
-                    , null
-                    , null);
+                , LocationEntry.TABLE_NAME
+                , new String[]{COLUMN_SELECTION, LocationEntry.COLUMN_DATE_TEXT}
+                , null
+                , null
+                , LocationEntry.COLUMN_DATE_TEXT
+                , null
+                , null
+                , null);
 
         for (int i =0; i< cursor.getCount(); i++){
              if (cursor.moveToNext()) {
@@ -81,10 +88,10 @@ public class ProductivityDBHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         ArrayList<DateCount> list = new ArrayList<>();
         Cursor cursor = db.rawQuery(
-                "select "+COLUMN_SELECTION+", "+
-                LocationEntry.COLUMN_CITY_NAME+"," +newColumn +
+                "select " + COLUMN_SELECTION + ", " +
+                        LocationEntry.COLUMN_CITY_NAME + "," + newColumn +
                         " as " + newColumnName +
-                " from "+LocationEntry.TABLE_NAME+" group by "+LocationEntry.COLUMN_CITY_NAME, null);
+                        " from " + LocationEntry.TABLE_NAME + " group by " + LocationEntry.COLUMN_CITY_NAME, null);
 
         for (int i =0; i< cursor.getCount(); i++){
             if (cursor.moveToNext()) {
@@ -98,5 +105,36 @@ public class ProductivityDBHelper extends SQLiteOpenHelper {
         cursor.close();
         db.close();
         return list;
+    }
+
+    public ArrayList<PointF> getDateLocations(String date) {
+        ArrayList<PointF> locations = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("select * from "+LocationEntry.TABLE_NAME+" where TRIM(date) = '" + date.trim() + "'", null);
+        if (cursor.moveToFirst()) {
+            while (!cursor.isAfterLast()) {
+                float lon = cursor.getFloat(cursor.getColumnIndex(LocationEntry.COLUMN_LONGITUDE));
+                float lat = cursor.getFloat(cursor.getColumnIndex(LocationEntry.COLUMN_LATITUDE));
+                cursor.moveToNext();
+                locations.add(new PointF(lat,lon));
+            }
+            cursor.close();
+        }
+        return locations;
+    }
+
+
+    public long saveLocationAndAddress(Location currentLocation, String address, int interval) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+        Date date = new Date();
+        String today = dateFormat.format(date);
+        ContentValues values = ProductivityContract.LocationEntry.createContentFromLocation(
+                currentLocation.getLongitude()
+                , currentLocation.getLatitude()
+                , address
+                , today, interval);
+        long rowId = db.insert(ProductivityContract.LocationEntry.TABLE_NAME, null, values);
+        return rowId;
     }
 }
